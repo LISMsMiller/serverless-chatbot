abb85e64d138b5f633aab672cb7e5474c8fd5aa6
Mike Nikles <mikenikles@gmail.com>
2018-12-16 01:17:51 +0000

    Log the event received in the function.

--- packages/reminder-bot-receiver/lib/reminder-bot-receiver.js
+++ packages/reminder-bot-receiver/lib/reminder-bot-receiver.js
@@ -1,4 +1,5 @@
 exports.reminderBotReceiver = (event, context) => {
+	console.log('Event received: ', event);
 	const pubSubMessage = event;
 	const name = pubSubMessage.data ?
 		Buffer.from(pubSubMessage.data, 'base64').toString() :
