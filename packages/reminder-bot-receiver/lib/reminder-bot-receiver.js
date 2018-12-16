08d2bb0471ebdb5a74d41f096e997fa1f22a4147
Mike Nikles <mikenikles@gmail.com>
2018-12-16 00:49:34 +0000

    Create the Pub/Sub topic, a subscription and the reminder-bot-receiver Cloud Function foundation.

---
+++ packages/reminder-bot-receiver/lib/reminder-bot-receiver.js
@@ -0,0 +1,8 @@
+exports.reminderBotReceiver = (event, context) => {
+	const pubSubMessage = event;
+	const name = pubSubMessage.data ?
+		Buffer.from(pubSubMessage.data, 'base64').toString() :
+		'World';
+
+	console.log(`Hello, ${name}!`);
+};
