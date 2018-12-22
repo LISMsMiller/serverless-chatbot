b9eb31e10fff68383922a5c2bc4752db25d49e09
Mike Nikles <mikenikles@gmail.com>
2018-12-22 20:27:04 +0000

    Call event handlers based on event type.

--- packages/reminder-bot-receiver/lib/reminder-bot-receiver.js
+++ packages/reminder-bot-receiver/lib/reminder-bot-receiver.js
@@ -1,9 +1,17 @@
-exports.reminderBotReceiver = (event, context) => {
-	console.log('Event received: ', event);
-	const pubSubMessage = event;
-	const name = pubSubMessage.data ?
-		Buffer.from(pubSubMessage.data, 'base64').toString() :
-		'World';
+const EVENT_HANDLERS = require('./event-handlers')
 
-	console.log(`Hello, ${name}!`);
+exports.reminderBotReceiver = async (event, context) => {
+  console.log(`Input received:
+    Event: ${JSON.stringify(event)}
+    Context: ${JSON.stringify(context)}`)
+
+  // See https://developers.google.com/hangouts/chat/reference/message-formats/events#event_fields
+  const chatEventBody = JSON.parse(Buffer.from(event.data, 'base64').toString())
+  console.log('Chat event body:', chatEventBody)
+
+  try {
+    EVENT_HANDLERS[chatEventBody.type]()
+  } catch (error) {
+    console.error(new Error(`Couldn't process event due to: ${error}`))
+  }
 };
