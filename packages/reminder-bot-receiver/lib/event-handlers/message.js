9301bf7d9f9b71b3c1a1a214a46986c0f42ca37c
Mike Nikles <mikenikles@gmail.com>
2018-12-22 21:23:55 +0000

    Determine the reminder subject.

--- packages/reminder-bot-receiver/lib/event-handlers/message.js
+++ packages/reminder-bot-receiver/lib/event-handlers/message.js
@@ -1,9 +1,26 @@
 const chrono = require('chrono-node')
 
+const determineReminderSubject = (userInput) => {
+  // Object reference at https://github.com/wanasit/chrono#usage
+  const parsedChrono = chrono.parse(userInput);
+  const dateTimeText = parsedChrono[0].text;
+
+  // Remove the date / time part of the user's message
+  let subject = userInput.replace(new RegExp(dateTimeText, 'g'), '');
+
+  // Remove "@Reminder Bot" if the bot is used in a room
+  subject = subject.replace(new RegExp('@Reminder Bot', 'g'), '');
+
+  return subject.trim();
+}
+
 module.exports = (chatEventBody) => {
   const userInput = chatEventBody.message.text
   console.log(`Processing event type MESSAGE for message text: ${userInput}`)
 
   const reminderDate = chrono.parseDate(userInput);
   console.log(`Found date: ${reminderDate}`)
+
+  const reminderSubject = determineReminderSubject(userInput)
+  console.log(`Found subject: ${reminderSubject}`)
 }
