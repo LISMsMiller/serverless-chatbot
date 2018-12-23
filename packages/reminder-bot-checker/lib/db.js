8046b0a5490b55eeb219d20543bb72b0f6b6295a
Mike Nikles <mikenikles@gmail.com>
2018-12-23 21:35:34 +0000

    Find reminders, print how many were found.

---
+++ packages/reminder-bot-checker/lib/db.js
@@ -0,0 +1,23 @@
+const Firestore = require('@google-cloud/firestore');
+
+const db = new Firestore({
+  projectId: process.env.GCP_PROJECT,
+  timestampsInSnapshots: true
+})
+
+const findReminders = async () => {
+  try {
+    const remindersRef = db.collection('reminders');
+    const queryRef = remindersRef
+      .where('remind_at', '<=', new Date())
+      .where('status', '==', 'new');
+    const querySnapshot = await queryRef.get();
+    return querySnapshot.docs;
+  } catch (error) {
+    console.error(new Error(`Reminders could not be found due to: ${error}`))
+  }
+}
+
+module.exports = {
+  findReminders
+}
