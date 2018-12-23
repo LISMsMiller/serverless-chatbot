a129ee52ba7788814820bf3f95a3d9f28074e508
Mike Nikles <mikenikles@gmail.com>
2018-12-23 20:38:55 +0000

    Add the reminder-bot-checker boilerplate code.

--- packages/reminder-bot-checker/lib/reminder-bot-checker.js
+++ packages/reminder-bot-checker/lib/reminder-bot-checker.js
@@ -1,7 +1,12 @@
-'use strict';
+exports.reminderBotChecker = async (event, context) => {
+  console.log(`Input received:
+    Event: ${JSON.stringify(event)}
+    Context: ${JSON.stringify(context)}`)
 
-module.exports = reminderBotChecker;
+  const schedulerBody = JSON.parse(Buffer.from(event.data, 'base64').toString())
+  console.log('Scheduler body:', schedulerBody) // Empty object
 
-function reminderBotChecker() {
-    // TODO
+  // TODO:
+  // * Check the database for reminders that are due
+  // * For each found reminder, publish a message to reminder-bot-messages-out
 }
